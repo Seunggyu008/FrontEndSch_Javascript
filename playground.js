39f559be74c88bbe3bfaa03d2ee83ae426d46466
seunggyu.jung <seunggyujung008@gmail.com>
2023-08-23 22:19:19 +0900

    20230823 수업 내용 업데이트

--- playground.js
+++ playground.js
@@ -1,3 +1,20 @@
-function createContextMenu (event) {
-    event.preventDefault();
+function solution(d, budget) {
+    let sortedD = [];
+    let answer = 0;
+
+
+    sortedD = d.sort((a, b) => a - b);
+
+    //정렬된 배열을 
+    for(let i = 0; i <= sortedD.length; i++) {
+        let dividedValue = sortedD[i] + sortedD[i+1]
+
+        if (dividedValue >= budget) {
+            break;
+        }
+        answer++;
+    }
+    return answer;
 }
+
+console.log(solution([1, 3, 2, 5 , 4], 9));
